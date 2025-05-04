9048d089c5be27513f7eff442cbe87cbc8d4d466
Akmal <kanselir8@gmail.com>
2025-05-04 10:12:06 +0700

    ncfx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -24,8 +23,8 @@ const nextConfig: NextConfig = {
   },
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/aiChat' : '',
-  trailingSlash: true,
   assetPrefix: process.env.NODE_ENV === 'production' ? '/aiChat' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
